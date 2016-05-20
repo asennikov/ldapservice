deedc43845efb72912b20ae68c4426b8831f7a8f
Alexander Sennikov <gseno88@gmail.com>
2016-05-20 21:10:09 +0300

    Save tokens to redis

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var jwt = require('jwt-simple');
 var moment = require('moment');
 var LdapAuth = require('ldapauth-fork');
 var cors = require('cors');
+var redis = require('redis');
 
 var app = express();
 
@@ -14,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cors());
 
-var options = {
+var auth = new LdapAuth({
   url: settings.url,
   searchBase: settings.searchBase,
   searchFilter: settings.searchFilter,
@@ -22,8 +23,13 @@ var options = {
   adminPassword: settings.admin.password,
   timeout: settings.timeout,
   connectTimeout: settings.connectTimeout
-};
-var auth = new LdapAuth(options);
+});
+
+var redisClient = redis.createClient({
+  host: settings.redis.host,
+  port: settings.redis.port,
+  password: settings.redis.password
+});
 
 app.set('jwtTokenSecret', settings.secret);
 
@@ -33,13 +39,16 @@ app.post('/authenticate', function(req, res) {
       if (err) {
         res.status(401).send({ error: 'Wrong user or password'});
       } else if (user) {
-        var expires = moment().add(7, 'days').valueOf();
+        var expirationInterval = moment.duration(7, 'days');
+        var expires = moment().add(expirationInterval).valueOf();
         var token = jwt.encode({
           exp: expires,
           username: user.sAMAccountName,
           groups: user.memberOf,
           mail: user.mail
         }, app.get('jwtTokenSecret'));
+
+        redisClient.setex(token, expirationInterval.asSeconds(), expires, redis.print);
 
         res.json({
           token : token,
